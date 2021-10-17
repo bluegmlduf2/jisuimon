21fcaa29b03752f76d14bf7a399bfe710478174e
wally_yoon <bluegmlduf2@naver.com>
2021-10-17 21:49:23 +0900

    build를 위한 vue.config.js 수정..이거때매..

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     configureWebpack: {
       devtool: 'source-map'
-    }
+    },
+    publicPath: "/jisuimon/"//서브디렉토리명으로 배포 localhost:8081/jisuimon  , baseUrl->publicPath 변경됨
   }
   
