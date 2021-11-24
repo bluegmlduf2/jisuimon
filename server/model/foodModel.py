e530e752f43be2c23b35f622380b4c6fd1238cda
wally_yoon <bluegmlduf2@naver.com>
2021-11-24 22:29:09 +0900

    1124 sql Ingection방어

--- server/model/foodModel.py
+++ server/model/foodModel.py
@@ -13,15 +13,15 @@ def getFood(args):
                 FROM
                     jisuimon.food_table AS FT
                 WHERE
-                    MATCH(FT.food_name) AGAINST("{food_name}*" IN BOOLEAN MODE)
+                    MATCH(FT.food_name) AGAINST("%s*" IN BOOLEAN MODE)
                 LIMIT 15
                 -- 검색어 4글자에서 2글자로
                 -- mysql --help | grep my.cnf 에서 ft_min_word_len=2추가
                 -- 공백구분가능한 fulltext인덱스 
                 -- ALTER TABLE jisuimon.food_table ADD FULLTEXT INDEX idx_foodName_full_text(food_name) WITH PARSER NGRAM
-            '''.format(food_name=args['food_name'])
+            '''
 
-            data = conn.executeAll(sql)
+            data = conn.executeAll(sql,args['food_name'])
         except UserError as e:
             return json.dumps({'status': False, 'message': e.msg}), 200
         except Exception as e:
