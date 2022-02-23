4eca0b64f35c9ea2176df298cd34151a43db41f4
wally_yoon <bluegmlduf2@naver.com>
2022-02-23 19:14:27 +0900

    0223 DB캐릭터셋변경

--- documents/jisuimon.sql
+++ documents/jisuimon.sql
@@ -25,14 +25,14 @@ DROP TABLE IF EXISTS `comment_reply_table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comment_reply_table` (
-  `comment_reply_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '대댓글id',
-  `comment_reply_content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '대댓글내용',
+  `comment_reply_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '대댓글id',
+  `comment_reply_content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '대댓글내용',
   `comment_reply_create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '대댓글 작성날짜',
   `comment_reply_update_date` datetime DEFAULT NULL,
   `user_table_user_id` varchar(36) NOT NULL COMMENT '대댓글 작성자 id',
   `comment_table_comment_id` varchar(36) NOT NULL COMMENT '댓글id',
   PRIMARY KEY (`comment_reply_id`,`user_table_user_id`,`comment_table_comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,16 +43,16 @@ DROP TABLE IF EXISTS `comment_table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comment_table` (
-  `comment_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '댓글id',
-  `comment_content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '댓글내용',
+  `comment_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '댓글id',
+  `comment_content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '댓글내용',
   `comment_create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '댓글등록날짜',
   `comment_update_date` datetime DEFAULT NULL,
-  `user_table_user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '댓글작성자id',
-  `post_table_post_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '게시물id',
+  `user_table_user_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '댓글작성자id',
+  `post_table_post_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '게시물id',
   PRIMARY KEY (`comment_id`,`user_table_user_id`,`post_table_post_id`),
   KEY `fk_comment_table_post_table1_idx` (`post_table_post_id`),
   KEY `fk_comment_table_user_table1_idx` (`user_table_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `food_table` (
   `food_name` varchar(100) NOT NULL,
   PRIMARY KEY (`food_id`),
   FULLTEXT KEY `idx_foodName_full_text` (`food_name`) /*!50100 WITH PARSER `ngram` */ 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,12 +78,12 @@ DROP TABLE IF EXISTS `ingredient_table`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ingredient_table` (
-  `post_table_post_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `post_table_post_id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `ingredient_id` int NOT NULL COMMENT 'food_id와 동일값',
   `ingredient_amt` int NOT NULL COMMENT '재료수량',
-  `ingredient_unit` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '재료단위',
+  `ingredient_unit` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '재료단위',
   PRIMARY KEY (`ingredient_id`,`post_table_post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,10 +96,10 @@ DROP TABLE IF EXISTS `post_detail_table`;
 CREATE TABLE `post_detail_table` (
   `post_detail_id` varchar(36) NOT NULL COMMENT '게시글상세id',
   `post_table_post_id` varchar(36) NOT NULL COMMENT '게시글id',
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '게시글내용 (2만자가량)',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '게시글내용 (2만자가량)',
   PRIMARY KEY (`post_detail_id`,`post_table_post_id`),
   KEY `fk_post_detail_table_post_table_idx` (`post_table_post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,7 +115,7 @@ CREATE TABLE `post_like_table` (
   PRIMARY KEY (`user_table_user_id`,`post_table_post_id`),
   KEY `fk_post_like_table_user_table1_idx` (`user_table_user_id`),
   KEY `fk_post_like_table_post_table1_idx` (`post_table_post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,14 +127,14 @@ DROP TABLE IF EXISTS `post_table`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `post_table` (
   `post_id` varchar(36) NOT NULL COMMENT '게시물id',
-  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '제목',
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '제목',
   `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '작성날짜',
   `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정날짜',
-  `title_image` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '제목이미지',
+  `title_image` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '제목이미지',
   `user_table_user_id` varchar(36) NOT NULL COMMENT '게시글작성자',
   PRIMARY KEY (`post_id`,`user_table_user_id`),
   KEY `fk_post_table_user_table1_idx` (`user_table_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,7 +147,7 @@ DROP TABLE IF EXISTS `user_table`;
 CREATE TABLE `user_table` (
   `user_id` varchar(36) NOT NULL COMMENT '유저id',
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
